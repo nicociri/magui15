6384f1349e4ddcee423f329afd18ad46c772d475
nicociri <basuradependragon@gmail.com>
2023-03-06 22:54:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,26 +3,20 @@ window.addEventListener('deviceorientation',function(e){
     const y = Math.round(e.gamma);
     const z = Math.round(e.alpha);
 
-    document.getElementsByClassName('xtxt')[0].innerText = "X-Axis = " + x;
-    document.getElementsByClassName('ytxt')[0].innerText = "Y-Axis = " + y;
-    document.getElementsByClassName('ztxt')[0].innerText = "Z-Axis = " + z;
-    
-     //restringimos el movimiento un poco
+    //restringimos el movimiento un poco
     if (x > 50) {
         x = 50;
       }
       if (x < -50) {
         x = -50;
       }
-    
-    if (y > 50) {
-        y = 50;
-      }
-      if (y < -50) {
-        y = -50;
-      }
+
+    document.getElementsByClassName('xtxt')[0].innerText = "X-Axis = " + x;
+    document.getElementsByClassName('ytxt')[0].innerText = "Y-Axis = " + y;
+    document.getElementsByClassName('ztxt')[0].innerText = "Z-Axis = " + z;
 
     //Parallax Effect
-    document.getElementsByClassName('borde')[0].style.transform = `translate(-${y}%,-${x}% )`;
+    document.getElementsByClassName('borde')[0].style.transform = `translate(-${y*0.25}%,-${x*0.25}%)`;
+    document.getElementsByClassName('magui')[0].style.transform = `translate(-${y*0.35}%,-${x*0.35}%)`;
     
 });
