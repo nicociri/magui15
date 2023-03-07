59ce66b65aa17ad836b3aab02f9faf648a32bf56
nicociri <basuradependragon@gmail.com>
2023-03-06 23:08:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,12 +11,21 @@ window.addEventListener('deviceorientation',function(e){
         x = -50;
       }
 
+         //restringimos el movimiento un poco
+    if (y > 50) {
+        y = 50;
+      }
+      if (y < -50) {
+        y = -50;
+      }
+
     document.getElementsByClassName('xtxt')[0].innerText = "X-Axis = " + x;
     document.getElementsByClassName('ytxt')[0].innerText = "Y-Axis = " + y;
     document.getElementsByClassName('ztxt')[0].innerText = "Z-Axis = " + z;
 
     //Parallax Effect
     document.getElementsByClassName('borde')[0].style.transform = `translate(-${y*0.25}%,-${x*0.25}%)`;
-    document.getElementsByClassName('magui')[0].style.transform = `translate(-${y*0.35}%,-${x*0.35}%)`;
+    document.getElementsByClassName('magui')[0].style.transform = `translate(-${y*0.45}%,-${x*0.45}%)`;
+    document.getElementsByClassName('fecha')[0].style.transform = `translate(-${y*0.65}%,-${x*0.65}%)`;
     
 });
