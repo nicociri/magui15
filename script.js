49add3c307c84451590fb4df91cd2793fbec9311
nicociri <basuradependragon@gmail.com>
2023-03-06 22:33:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ window.addEventListener('deviceorientation',function(e){
     document.getElementsByClassName('xtxt')[0].innerText = "X-Axis = " + x;
     document.getElementsByClassName('ytxt')[0].innerText = "Y-Axis = " + y;
     document.getElementsByClassName('ztxt')[0].innerText = "Z-Axis = " + z;
+    
+     //restringimos el movimiento un poco
+    if (x > 50) {
+        x = 50;
+      }
+      if (x < -50) {
+        x = -50;
+      }
 
     //Parallax Effect
-
-    document.getElementsByClassName('borde')[0].style.transform = `translateY(-${x*0.25}%)`;
-    document.getElementsByClassName('borde')[0].style.transform = `translateX(-${y*0.25}%)`;
-
+    document.getElementsByClassName('borde')[0].style.transform = `translate(-${x}%,-${y}% )`;
+    
 });
